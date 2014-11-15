8012f78b657f0e80a8b1aa336b2994df2a4a7f32
scyclow <spike716@gmail.com>
2014-11-15 15:28:24 -0600

    changed exercise/food routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :foods, except: [:new, :edit]
-  resources :exercises, except: [:new, :edit]
   resources :trainees, except: [:new, :edit]
-  resources :trainers, except: [:new, :edit]
+  resources :trainers, except: [:new, :edit] do
+    resources :foods, except: [:new, :edit]
+    resources :exercises, except: [:new, :edit]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
